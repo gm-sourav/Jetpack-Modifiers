e3fee58d34cf3c476423079e9fb0875950dd5143
gm-sourav <souravmahbub13@gmail.com>
2026-04-11 18:08:16 +0600

    update change

---
+++ app/src/main/java/com/example/jetpack_modifiers/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.jetpack_modifiers
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpack_modifiers.ui.theme.JetpackModifiersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackModifiersTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackModifiersTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/jetpack_modifiers/Task1.kt
@@ -0,0 +1,10 @@
+package com.example.jetpack_modifiers
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+ fun Modi () {
+
+}
